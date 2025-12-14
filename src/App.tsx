4c7159d3614e329ad9e3f5580e266581ce2340e0
Kasfi Ahamed <kasfikas@gmail.com>
2025-12-14 13:53:33 +0000

    Add scroll to top on navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Layout } from './layout/Layout';
+import { ScrollToTop } from './components/ScrollToTop';
 import { Home } from './pages/Home';
 import { Projects } from './pages/Projects';
 import { ProjectDetail } from './pages/ProjectDetail';
@@ -12,6 +13,7 @@ import { NotFound } from './pages/NotFound';
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <Layout>
         <Routes>
           <Route path="/" element={<Home />} />
